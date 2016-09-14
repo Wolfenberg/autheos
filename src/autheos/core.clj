fa8586a9fb4572f725e349e302f9331e0a04092c
Dmitry Volkov <dmitry@afrogleap.com>
2016-09-14 20:58:25 +0200

    step 2

--- src/autheos/core.clj
+++ src/autheos/core.clj
@@ -4,5 +4,13 @@
             [mikera.image.spectrum ]))
 
 
+(defn image-bytes
+  "Loads an image and returns an int array"
+  [path]
+  (-> (i/load-image path)
+      ((f/grayscale))
+      (i/resize 20 20)
+      (i/get-pixels)))
+
 (let [image (i/load-image "banana.bmp")]
-  (i/show (i/resize ((f/grayscale) image) 20 20)))
+  (apply max (vec (i/get-pixels (i/resize ((f/grayscale) image) 20 20)))))
